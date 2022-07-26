3ea8f9795620443389cd1d79b90442a7d904066c
Mrinal-K-Mishra <107672054+Mrinal-K-Mishra@users.noreply.github.com>
2022-07-26 10:45:42 +0530

    Update ArraySpecificValue.java

--- ArraySpecificValue.java
+++ ArraySpecificValue.java
@@ -12,6 +12,6 @@ public static void main(String[] args) {
 int arr1[] = {1789, 2035, 1899, 1456, 2013,2022,1958, 2295, 1971, 2950};   //initialize array 
 
 System.out.println(contains(arr1, 2013));      //array output initialisation
-System.out.println(contains(arr1, 2015));      
+System.out.println(contains(arr1, 2015));      //array output initialisation     
 }
 }
