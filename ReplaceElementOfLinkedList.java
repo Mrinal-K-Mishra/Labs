3ae6f046934898d3433489edd7190ef3b15828b3
Mrinal-K-Mishra <107672054+Mrinal-K-Mishra@users.noreply.github.com>
2022-08-12 20:11:49 +0530

    Update ReplaceElementOfLinkedList.java

--- ReplaceElementOfLinkedList.java
+++ ReplaceElementOfLinkedList.java
@@ -3,11 +3,11 @@ import java.util.LinkedList;
 public class ReplaceElementOfLinkedList {
 		public static void main(String ...args) {
 			LinkedList<Integer> ll = new LinkedList<>();
-			ll.add(15);
-			ll.add(30);
-			ll.add(28);
-			ll.add(68);
-			ll.add(45);
+			ll.add(19);
+			ll.add(53);
+			ll.add(26);
+			ll.add(98);
+			ll.add(55);
 			// before replacing element
 			System.out.println(ll);
 			ll.remove(); // this method will remove the first element 
