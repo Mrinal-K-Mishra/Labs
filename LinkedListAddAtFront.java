4fc555613dee7ea828d5335806be413a78cff7f7
Mrinal-K-Mishra <107672054+Mrinal-K-Mishra@users.noreply.github.com>
2022-08-12 20:10:32 +0530

    Update LinkedListAddAtFront.java

--- LinkedListAddAtFront.java
+++ LinkedListAddAtFront.java
@@ -3,14 +3,14 @@ import java.util.LinkedList;
 public class LinkedListAddAtFront {
 		public static void main(String ...args) {
 			LinkedList<Integer> ll = new LinkedList<>();
-			ll.add(15);
-			ll.add(28);
-			ll.add(30);
-			ll.add(68);
-			ll.add(45);
+			ll.add(19);
+			ll.add(46);
+			ll.add(35);
+			ll.add(98);
+			ll.add(55);
 			// before adding element at front
 			System.out.println(ll);
-			ll.addFirst(6000); // adding an element(1000) in front of the linked list
+			ll.addFirst(8000); // adding an element(8000) in front of the linked list
 			// after adding element at front
 			System.out.println(ll);
 		}
